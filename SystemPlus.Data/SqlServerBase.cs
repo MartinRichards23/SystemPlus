b240a58933dbd0499696c40fe3663782a8fa74c5
Martin Richards <martinrichards23@gmail.com>
2019-06-06 14:28:19 +0100

    added systemplus.data

---
+++ SystemPlus.Data/SqlServerBase.cs
@@ -0,0 +1,68 @@
+﻿using System.Data.SqlClient;
+
+namespace SystemPlus.Data
+{
+    /// <summary>
+    /// Base class for Sql Server database api
+    /// </summary>
+    public abstract class SqlServerBase
+    {
+        protected SqlServerBase(string conString)
+        {
+            ConString = conString;
+        }
+
+        protected string ConString
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets an open SqlConnection
+        /// </summary>
+        public virtual SqlConnection GetConnection()
+        {
+            SqlConnection cn = new SqlConnection(ConString);
+            cn.Open();
+            return cn;
+        }
+
+        public void TestConnection()
+        {
+            SqlConnection con = null;
+
+            try
+            {
+                con = GetConnection();
+            }
+            finally
+            {
+                if (con != null)
+                    con.Dispose();
+            }
+        }
+
+        public static string MakeConString(string address, string dbName, bool windowsAuth, string userName, string password, int timeout = 60)
+        {
+            SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = address,
+                IntegratedSecurity = windowsAuth,
+                PersistSecurityInfo = true,
+                Pooling = true,
+                MultipleActiveResultSets = true,
+                ConnectTimeout = timeout
+            };
+
+            if (!string.IsNullOrEmpty(userName))
+                sqlBuilder.UserID = userName;
+            if (!string.IsNullOrEmpty(password))
+                sqlBuilder.Password = password;
+
+            if (!string.IsNullOrWhiteSpace(dbName))
+                sqlBuilder.InitialCatalog = dbName;
+
+            return sqlBuilder.ConnectionString;
+        }
+    }
+}
---
+++ SystemPlus.Data/SqlTable.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using SystemPlus.Text;
+
+namespace SystemPlus.Data
+{
+    public class SqlTable
+    {
+        public SqlTable(string name)
+        {
+            Name = name;
+            ClassName = Name.ToUpperFirst().RemovePlural();
+            InstanceName = ClassName.ToLowerFirst();
+        }
+
+        public string Name { get; }
+        public List<SqlColumn> Columns { get; } = new List<SqlColumn>();
+
+        public string ClassName { get; }
+        public string InstanceName { get; }
+
+        public IEnumerable<SqlColumn> PrimaryKeyColumns
+        {
+            get { return Columns.Where(c => c.IsPrimaryKey); }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class SqlColumn
+    {
+        public string Name { get; set; }
+        public SqlDbType DataType { get; set; }
+        public bool Computed { get; set; }
+        public int Length { get; set; }
+        public bool Nullable { get; set; }
+
+        public bool IsPrimaryKey { get; set; }
+        public bool IsIdentity { get; set; }
+
+        public void SetValues()
+        {
+            PropertyName = Name.ToUpperFirst();
+            InstanceName = Name.ToLowerFirst();
+            PropertyType = SqlTools.SqlDbTypeToType(DataType);
+            string propertyTypeName = SqlTools.SqlDbTypeToTypeName(DataType);
+
+            if (Nullable && !PropertyType.IsNullable())
+                propertyTypeName += "?";
+
+            PropertyTypeName = propertyTypeName;
+
+            SqlParamName = "@" + Name.ToLowerFirst();
+            SqlDataType = SqlTools.GetSqlDataTypeName(DataType, Length);
+        }
+
+        public string PropertyName { get; set; }
+        public string InstanceName { get; set; }
+        public Type PropertyType { get; set; }
+        public string PropertyTypeName { get; set; }
+        public string SqlParamName { get; set; }
+        public string SqlDataType { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}

74b4d9ecbf89cb3931b061550904a86df23ee3e2
Martin Richards <martinrichards23@gmail.com>
2019-06-08 13:23:03 +0100

    m

---
+++ SystemPlus.Web/Logging/EmailLogger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SystemPlus.Web.Email;
+using Microsoft.Extensions.Logging;
+
+namespace SystemPlus.Web.Logging
+{
+    public class EmailLoggerProvider : ILoggerProvider
+    {
+        IEmailSender emailer;
+        string address;
+
+        public EmailLoggerProvider(IEmailSender emailer, string address)
+        {
+            this.emailer = emailer;
+            this.address = address;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new EmailLogger(emailer, address);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+
+    class EmailLogger : ILogger
+    {
+        IEmailSender emailer;
+        string address;
+
+        public EmailLogger(IEmailSender emailer, string address)
+        {
+            this.emailer = emailer;
+            this.address = address;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel >= LogLevel.Error;
+        }
+
+        public async void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (logLevel >= LogLevel.Error && exception != null)
+            {
+                try
+                {
+                    await emailer.SendEmailAsync(address, "Error", exception.ToString(), false);
+                }
+                catch
+                { }
+            }
+        }
+    }
+}
---
+++ SystemPlus.Web/Logging/LoggingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemPlus.Web.Logging
+{
+    public static class LoggingExtensions
+    {
+        public static Task ContinueWithLogErrors(this Task task, ILogger logger, [CallerMemberName] string message = "")
+        {
+            return task.ContinueWith(t =>
+            {
+                if (t.Exception != null)
+                    logger.LogError(t.Exception, message);
+            }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}

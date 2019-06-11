25598c08c1fbd325fac636cb1810f56de4e4b6af
Martin Richards <martinrichards23@gmail.com>
2019-06-11 11:29:45 +0100

    m

--- SystemPlus.Web/HttpExtensions.cs
+++ SystemPlus.Web/HttpExtensions.cs
@@ -7,19 +7,19 @@ namespace SystemPlus.Web
 {
     public static class HttpExtensions
     {
-        //public static string UserAgent(this HttpRequest request)
-        //{
-        //    return request.Headers["User-Agent"];
-        //}
+        public static string UserAgent(this HttpRequest request)
+        {
+            return request.Headers["User-Agent"];
+        }
 
-        //public static string Referer(this HttpRequest request)
-        //{
-        //    return request.Headers["Referer"];
-        //}
+        public static string Referer(this HttpRequest request)
+        {
+            return request.Headers["Referer"];
+        }
 
-        //public static string GetIpAddress(this HttpContext context)
-        //{
-        //    return context.Connection.RemoteIpAddress?.ToString();
-        //}
+        public static string GetIpAddress(this HttpContext context)
+        {
+            return context.Connection.RemoteIpAddress?.ToString();
+        }
     }
 }

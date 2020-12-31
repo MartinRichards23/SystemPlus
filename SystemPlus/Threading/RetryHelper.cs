4542c4677554b1140b14aa0e7373f3b37fc4bec8
Martin Richards <39667281+MartinRichards23@users.noreply.github.com>
2020-12-31 11:39:23 +0000

    TaskExtensions.TimeoutAfter()

---
+++ SystemPlus/Threading/RetryHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SystemPlus.Threading
+{
+    public static class RetryHelper
+    {
+        /// <summary>
+        /// Retries the operation until it fails and throws the exception
+        /// </summary>
+        /// <param name="maxAttempts">Max number of times to try</param>
+        /// <param name="delay">Delay between each attempt</param>
+        /// <param name="operation">The task to run</param>
+        public static async Task<T> RetryOnException<T>(int maxAttempts, TimeSpan delay, Func<Task<T>> operation)
+        {
+            if (operation == null)
+                throw new ArgumentNullException(nameof(operation));
+
+            int attempts = 0;
+
+            do
+            {
+                try
+                {
+                    attempts++;
+                    return await operation();
+                }
+                catch
+                {
+                    if (attempts >= maxAttempts)
+                        throw;
+
+                    // delay before next attempt
+                    await Task.Delay(delay);
+                }
+            } while (true);
+        }
+
+        /// <summary>
+        /// Retries the operation until it fails and throws the exception
+        /// </summary>
+        /// <param name="maxAttempts">Max number of times to try</param>
+        /// <param name="delay">Delay between each attempt</param>
+        /// <param name="operation">The task to run</param>
+        public static async Task RetryOnException(int maxAttempts, TimeSpan delay, Func<Task> operation)
+        {
+            if (operation == null)
+                throw new ArgumentNullException(nameof(operation));
+
+            int attempts = 0;
+
+            do
+            {
+                try
+                {
+                    attempts++;
+                    await operation();
+                    return;
+                }
+                catch
+                {
+                    if (attempts >= maxAttempts)
+                        throw;
+
+                    // delay before next attempt
+                    await Task.Delay(delay);
+                }
+            } while (true);
+        }
+    }
+}
--- SystemPlus/Threading/TaskExtensions.cs
+++ SystemPlus/Threading/TaskExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SystemPlus.Threading
 {
@@ -7,6 +9,27 @@ namespace SystemPlus.Threading
     /// </summary>
     public static class TaskExtensions
     {
+        /// <summary>
+        /// Await a Task with a timeout
+        /// </summary>
+        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            using (CancellationTokenSource timeoutCancellationTokenSource = new CancellationTokenSource())
+            {
+                Task completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+
+                if (completedTask == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    return await task;  // propagate exceptions
+                }
+                else
+                {
+                    throw new TimeoutException();
+                }
+            }
+        }
+
         /// <summary>
         /// Allows an async function to be called and not awaited without the warning.
         /// </summary>

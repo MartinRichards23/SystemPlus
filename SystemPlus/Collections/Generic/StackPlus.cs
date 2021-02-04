4cc3250d1866ed90644a36be458e4ae8971d9963
Martin Richards <39667281+MartinRichards23@users.noreply.github.com>
2021-02-04 14:23:19 +0000

    added StackPlus

---
+++ SystemPlus/Collections/Generic/StackPlus.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SystemPlus.Collections.Generic
+{
+    /// <summary>
+    /// A Stack implementation with extra funcationality
+    /// </summary>
+    public class StackPlus<T> : IEnumerable<T>
+    {
+        readonly List<T> items = new List<T>();
+
+        public int Count { get { return items.Count; } }
+
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+
+        public T? Pop()
+        {
+            if (items.Count > 0)
+            { 
+                int index = items.Count - 1;
+                return Remove(index);
+            }
+
+            return default(T);
+        }
+
+        public T Remove(int index)
+        {
+            T item = items[index];
+            items.RemoveAt(index);
+
+            return item;
+        }
+
+        public void RemoveOldest(int maxSize)
+        {
+            while(items.Count > maxSize)
+            {
+                items.RemoveAt(0);
+            }
+        }
+
+        public void Clear()
+        {
+            items.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)items).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)items).GetEnumerator();
+        }
+    }
+}

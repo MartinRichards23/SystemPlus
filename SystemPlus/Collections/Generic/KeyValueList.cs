9daa6c9cf6c1d5b57a16ef5cbe219b3229906ae5
Martin Richards <39667281+MartinRichards23@users.noreply.github.com>
2023-01-11 11:18:17 +0000

    Added KeyValueList

---
+++ SystemPlus/Collections/Generic/KeyValueList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemPlus.Collections.Generic
+{
+    public class KeyValueList<TKey, TValue> : List<KeyValuePair<TKey, TValue>>
+    {
+        public void AddValues(TKey key, TValue value)
+        {
+            KeyValuePair<TKey, TValue> kvp = new KeyValuePair<TKey, TValue>(key, value);
+            Add(kvp);
+        }
+    }
+}

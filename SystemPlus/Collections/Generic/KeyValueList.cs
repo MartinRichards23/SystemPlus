8b8845c9c48f1cf7dbdcb8dd00ff0bb4ea50c068
Martin Richards <39667281+MartinRichards23@users.noreply.github.com>
2023-01-11 14:25:51 +0000

    improved KeyValueList

--- SystemPlus/Collections/Generic/KeyValueList.cs
+++ SystemPlus/Collections/Generic/KeyValueList.cs
@@ -6,12 +6,24 @@ using System.Threading.Tasks;
 
 namespace SystemPlus.Collections.Generic
 {
-    public class KeyValueList<TKey, TValue> : List<KeyValuePair<TKey, TValue>>
+    public class KeyValueList<TKey, TValue> : List<KeyValuePair<TKey, TValue>> where TKey : notnull
     {
-        public void AddValues(TKey key, TValue value)
+        public void Add(TKey key, TValue value)
         {
             KeyValuePair<TKey, TValue> kvp = new KeyValuePair<TKey, TValue>(key, value);
             Add(kvp);
+        }
+
+        public Dictionary<TKey, TValue> ToDictionary()
+        {
+            Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+
+            foreach (var kvp in this)
+            {
+                dictionary.TryAdd(kvp.Key, kvp.Value);
+            }
+
+            return dictionary;
         }
     }
 }

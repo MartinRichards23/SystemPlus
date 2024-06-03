8124dc0e49928028a47516d876bb5214da8941ab
Martin Richards <39667281+MartinRichards23@users.noreply.github.com>
2024-06-03 17:01:19 +0100

    Added ObservableCollection ProcessChangedEvent

--- SystemPlus/Collections/ObjectModel/ObjectModelExtensions.cs
+++ SystemPlus/Collections/ObjectModel/ObjectModelExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace SystemPlus.Collections.ObjectModel
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace SystemPlus.Collections.ObjectModel
 {
     public static class ObjectModelExtensions
     {
@@ -24,6 +27,59 @@
             items.AddRange(list);
 
             return items;
+        }
+
+        public static void ProcessChangedEvent<T>(this ObservableCollection<T> target, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    for (int i = 0; i < e.NewItems.Count; i++)
+                    {
+                        target.Insert(e.NewStartingIndex + i, (T)e.NewItems[i]);
+                    }
+                    break;
+
+                case NotifyCollectionChangedAction.Move:
+                    if (e.OldItems.Count == 1)
+                    {
+                        target.Move(e.OldStartingIndex, e.NewStartingIndex);
+                    }
+                    else
+                    {
+                        List<T> items = target.Skip(e.OldStartingIndex).Take(e.OldItems.Count).ToList();
+                        for (int i = 0; i < e.OldItems.Count; i++)
+                            target.RemoveAt(e.OldStartingIndex);
+
+                        for (int i = 0; i < items.Count; i++)
+                            target.Insert(e.NewStartingIndex + i, items[i]);
+                    }
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    for (int i = 0; i < e.OldItems.Count; i++)
+                        target.RemoveAt(e.OldStartingIndex);
+                    break;
+
+                case NotifyCollectionChangedAction.Replace:
+                    // remove
+                    for (int i = 0; i < e.OldItems.Count; i++)
+                        target.RemoveAt(e.OldStartingIndex);
+
+                    // add
+                    goto case NotifyCollectionChangedAction.Add;
+
+                case NotifyCollectionChangedAction.Reset:
+                    target.Clear();
+
+                    for (int i = 0; i < e.NewItems.Count; i++)
+                        target.Add((T)e.NewItems[i]);
+
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
